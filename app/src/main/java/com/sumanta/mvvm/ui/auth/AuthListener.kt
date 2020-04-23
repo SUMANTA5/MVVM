322e379af1be3d47d3c77b1da156559c253da8b2
sumanta <sumanta111195@gmail.com>
2020-04-23 07:36:16 +0530

    MVVM

---
+++ app/src/main/java/com/sumanta/mvvm/ui/auth/AuthListener.kt
@@ -0,0 +1,12 @@
+package com.sumanta.mvvm.ui.auth
+
+import androidx.lifecycle.LiveData
+
+interface AuthListener {
+
+    fun onStarted()
+
+    fun onSuccess(loginResponse: LiveData<String>)
+
+    fun onFailure(message: String)
+}
